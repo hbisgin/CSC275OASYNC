510b7f01d393d94729129973dc5868fa11b4ef80
Halil Bisgin <halilbisgin@gmail.com>
2025-01-09 09:40:21 -0500

    test code

---
+++ TestFolder/testMain.cpp
@@ -0,0 +1,18 @@
+//
+// Created by bisgin on 1/9/2025.
+//
+
+#include<iostream>
+
+using namespace std;
+
+struct Fruit{
+   int a, b;
+};
+
+int main()
+   {
+   Fruit fruit;
+   fruit.a = 1;
+   cout<<"Hello World!"<< " "<<fruit.a<<endl;
+   }
